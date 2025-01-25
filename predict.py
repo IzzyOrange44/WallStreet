4c1c807918cf138683cf5487e99fe91a8fa736d9
Isaac Arnold <arnoldaisaac@icloud.com>
2025-01-24 19:34:12 -0500

    add Postgres

--- predict.py
+++ predict.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI','sqlite:///users.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
